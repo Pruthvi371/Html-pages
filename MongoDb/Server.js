a1f7a59d5860149c1b1313bedf4a1d79b7730c68
Pruthvi371 <pruthvi371@gmail.com>
2025-06-23 00:03:27 +0530

    Update Server.js

--- MongoDb/Server.js
+++ MongoDb/Server.js
@@ -6,6 +6,7 @@ let connectToMDB=async()=>{
     try{
 await connect("mongodb+srv://pruthvi371:371Gmail@clusterpp.lz9hfrm.mongodb.net/?retryWrites=true&w=majority&appName=clusterpp");
 console.log("Succesfully Conected to MDB");
+        <h1>Code is added by PP</h1>
     }
     catch(err){
 console.log("got an error");
